c55e6bd3a16aad57f7850a7a8bce4e410ab30874
ram MANO <ram MANO>
2017-03-05 12:47:00 -0500

    changed initialize.php so it contrains prj666 in directory

--- photo_gallery/includes/initialize.php
+++ photo_gallery/includes/initialize.php
@@ -1,6 +1,6 @@
 <?php
 defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
-defined('SITE_ROOT') ? null : define('SITE_ROOT', 'C:'.DS.'wamp'.DS.'www'.DS.'photo_gallery');
+defined('SITE_ROOT') ? null : define('SITE_ROOT', 'C:'.DS.'wamp'.DS.'www'.DS.'prj666'.DS.'photo_gallery');
 defined('INC_PATH') ? null : define('INC_PATH', SITE_ROOT.DS.'includes');
 
 require_once(INC_PATH.DS."config.php");
